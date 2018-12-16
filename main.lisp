21ac0169265e716c79576e3507f89bf6d921844c
ivanica29 <ivana.96vr@gmail.com>
2018-12-16 20:32:05 +0100

    min max

--- main.lisp
+++ main.lisp
@@ -699,8 +699,67 @@
 	)	
 		
 )
+
+;=================
+;MIN-MAX
+;=============
+
 (defun vrati_poziciju(xi xj)
 	(list(nadjiSlovo xi) xj )
 )
+
+
+(defun getAssessment(state)
+    (random 100)
+)         
+
+(defun minimax (state depth alpha beta currentPlayer)
+    (cond
+        ((zerop depth)  (list state (getAssessment state)))
+        (t   
+            (let (
+                    (lp (returnPossibleStates state))
+                    (f  (if (string= "X" currentPlayer) 'max-state 'min-state))
+                )
+                (cond 
+                    ( (null lp) (list state (getAssessment state)))
+                    ( t (apply f (list lp depth alpha beta currentPlayer '())) )
+                )
+            )
+        )
+    )
+)
+
+(defun min-state (lp depth alpha beta currentPlayer state)
+    (cond
+        ( ( null lp) (list state beta) )
+        ( t 
+            (let* 
+                (
+                    (maxState (minimax (car lp) (1- depth) alpha beta (if (string= currentPlayer "X") "O"  "X")))
+                    (nb (min beta (cadr maxState)))
+                    (newState (if (< nb beta) (car lp) state))
+                )
+                (if  (> nb alpha) (min-state (cdr lp) depth alpha nb currentPlayer newState) (list newState nb) )
+            )
+        )
+    )
+)
+
+(defun max-state(lp depth alpha beta currentPlayer state)
+    (cond
+        ( (null lp) (list state alpha) )
+        ( t 
+            (let*
+                (
+                    (minState (minimax (car lp) (1- depth) alpha beta (if (string= currentPlayer "X") "O"  "X")))
+                    (na (max alpha (cadr minState)))
+                    (newState (if (> na alpha)  (car lp) state))
+                )
+                (if (< na beta) (max-state (cdr lp) depth na beta currentPlayer newState) (list newState na))
+            )
+        )
+    )
+)        
 
 (start)
